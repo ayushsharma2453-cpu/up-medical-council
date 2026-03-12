8f75b3b1c801381bfbf88bf19aeeedf0630f5efe
ayushsharma2453-cpu <ayushsharma2453@gmail.com>
2026-03-12 23:54:34 +0530

    changes

--- server.js
+++ server.js
@@ -1,41 +1,15 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
-app.use(express.json());
+app.use(express.static(path.join(__dirname,"public")));
 
-let doctors = [
-{
-id:1,
-year:2021,
-regno:"123456",
-state:"Uttar Pradesh",
-name:"Dr Example",
-father:"Mr Example"
-},
-{
-id:2,
-year:2020,
-regno:"654321",
-state:"Delhi",
-name:"Dr Rahul",
-father:"Mr Sharma"
-}
-];
-
-app.get("/search", (req,res)=>{
-
-const name = req.query.name;
-
-const result = doctors.filter(d =>
-d.name.toLowerCase().includes(name.toLowerCase())
-);
-
-res.json(result);
-
+app.get("/", (req,res)=>{
+res.sendFile(path.join(__dirname,"public","index.html"));
 });
 
-app.listen(PORT,()=>{
-console.log("Server running on port "+PORT);
+app.listen(PORT, ()=>{
+console.log("Server running");
 });
