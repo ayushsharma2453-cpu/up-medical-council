c71c73d104a88e2e6972d20385906f8920766b8d
ayushsharma2453-cpu <ayushsharma2453@gmail.com>
2026-03-12 23:21:47 +0530

    Changes

--- public/server.js
+++ public/server.js
@@ -1,34 +1,23 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
 
-app.use(express.static("public"));
-app.use(express.json());
+app.use(express.static(__dirname));
 
-let doctors = [
+const doctors = [
 {
-id:1,
 year:2021,
 regno:"123456",
 state:"Uttar Pradesh",
 name:"Dr Example",
 father:"Mr Example"
-},
-{
-id:2,
-year:2020,
-regno:"654321",
-state:"Delhi",
-name:"Dr Rahul",
-father:"Mr Sharma"
 }
 ];
 
-app.get("/search", (req,res)=>{
+app.get("/search",(req,res)=>{
 
-const name = req.query.name;
+let name = req.query.name;
 
-const result = doctors.filter(d =>
+let result = doctors.filter(d =>
 d.name.toLowerCase().includes(name.toLowerCase())
 );
 
@@ -36,6 +25,6 @@ res.json(result);
 
 });
 
-app.listen(PORT,()=>{
-console.log("Server running on port "+PORT);
+app.listen(3000,()=>{
+console.log("Server running on port 3000");
 });
